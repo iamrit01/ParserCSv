f1f0173a83e316f9b3b9de0fdacaa820876b3ed0
Bikry <bikry@Bikrys-MacBook-Air.local>
2022-10-09 02:30:46 +0530

    add threads

--- src/main/java/com/example/orders_parser/OrdersParserApplication.java
+++ src/main/java/com/example/orders_parser/OrdersParserApplication.java
@@ -15,57 +15,69 @@ import java.util.stream.Collectors;
 public class OrdersParserApplication {
 
 	public static void main(String[] args) throws IOException {
+
 		for(int i=0; i< args.length; i++){
+
 			if(args[i].contains(".csv")){
-				try (BufferedReader in = new BufferedReader(new FileReader("/Users/bikry/Desktop/"+args[i]))) {
 
-					List<Orders> orders = in.lines().skip(1).map(line -> {
-						String[] x = line.split(" ",4);
-						return new Orders(Integer.parseInt(x[0]), Integer.parseInt(x[1]), x[2], x[3].replace("\"","").trim());
-					}).collect(Collectors.toList());
+				String file = args[i];
 
-					int line = 1;
-					for(Orders orders1: orders){
-						orders1.setLine(line);
-						orders1.setFilename(args[i]);
-						orders1.setResult("OK");
-						System.out.println(orders1);
-						line++;
+				new Thread(){
+					public void run(){
+						try (BufferedReader in = new BufferedReader(new FileReader("/Users/bikry/Desktop/"+file))) {
+
+							List<Orders> orders = in.lines().skip(1).map(line -> {
+								String[] x = line.split(" ",4);
+								return new Orders(Integer.parseInt(x[0]), Integer.parseInt(x[1]), x[2], x[3].replace("\"","").trim());
+							}).collect(Collectors.toList());
+
+							int line = 1;
+							for(Orders orders1: orders){
+								orders1.setLine(line);
+								orders1.setFilename(file);
+								orders1.setResult("OK");
+								System.out.println(orders1);
+								line++;
+							}
+
+
+						} catch (StreamWriteException e) {
+							e.printStackTrace();
+						}  catch (IOException e) {
+							e.printStackTrace();
+						}
 					}
-
-
-				} catch (StreamWriteException e) {
-					e.printStackTrace();
-				}  catch (IOException e) {
-					e.printStackTrace();
-				}
+				}.start();
 			}
 			else if(args[i].contains(".json")){
+				String file= args[i];
+				new Thread(){
+					public void run(){
+						try (FileReader reader = new FileReader("/Users/bikry/Desktop/"+file))
+						{
+							JSONParser parser = new JSONParser();
+							Object obj = parser.parse(reader);
 
-				try (FileReader reader = new FileReader("/Users/bikry/Desktop/"+args[i]))
-				{
-					JSONParser parser = new JSONParser();
-					Object obj = parser.parse(reader);
+							JSONArray orderList = (JSONArray) obj;
+							int line = 1;
+							for(Object order: orderList) {
+								order = (JSONObject) order;
+								((JSONObject) order).put("filename", file);
+								((JSONObject) order).put("line", line);
+								((JSONObject) order).put("result", "OK");
+								System.out.println(order);
+								line++;
+							}
 
-					JSONArray orderList = (JSONArray) obj;
-					int line = 1;
-					for(Object order: orderList) {
-						order = (JSONObject) order;
-						((JSONObject) order).put("filename", args[i]);
-						((JSONObject) order).put("line", line);
-						((JSONObject) order).put("result", "OK");
-						System.out.println(order);
-						line++;
+						} catch (FileNotFoundException e) {
+							e.printStackTrace();
+						} catch (IOException e) {
+							e.printStackTrace();
+						} catch (ParseException e) {
+							e.printStackTrace();
+						}
 					}
-
-				} catch (FileNotFoundException e) {
-					e.printStackTrace();
-				} catch (IOException e) {
-					e.printStackTrace();
-				} catch (ParseException e) {
-					e.printStackTrace();
-				}
-
+				}.start();
 			}
 		}
 
